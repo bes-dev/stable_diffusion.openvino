6c594333f6b5692eb9d86c958c0b649d4869bce5
Сергей Белоусов <s.belousov@aiby.com>
2023-01-12 20:01:37 +0300

    remove deprecated files

--- sd-web.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-docker run -p 9090:8501 sd-web
--- sd.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set CDIR="%~dp0"
-docker run -v %CDIR%:/tmp sd --prompt "Emilia Clake drinking a coffee" --output /tmp/result.png
